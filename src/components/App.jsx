8935e8c363a4f9c22f502d5a90b27ca6e3d4d11a
Sabrina-Ibrahim <Sabrina.ibrahim24@me.com>
2022-01-26 21:09:01 -0600

    Create React App.

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+//import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <h1>our Youtube Project</h1>
+        )
+    }
+}
+
+export default App;
