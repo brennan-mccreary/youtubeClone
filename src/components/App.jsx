66d2adf83fab7ea37eabcaab3e7c69904c8c2e2e
Brennan McCreary <brennanmccreary@gmail.com>
2022-01-27 19:24:16 -0500

    Install Axios and establish test connection to backend.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,32 @@
 //import { render } from '@testing-library/react';
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            
+        };
     }
 
+    //HTTP Requests
+    ////GET Initial Test
+    getInit = async () => {
+        await axios
+            .get('http://localhost:5002/')
+            .then((res) => {
+                console.log(res.data);
+            })
+    };
+
+    //Run When Component Initially Mounts
+    componentDidMount() {
+        this.getInit();
+    };
+
+    //Render
     render() {
         return (
             <h1>our Youtube Project</h1>
