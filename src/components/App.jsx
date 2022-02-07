a3666bd55565ded8241341e1ad7a527b60eae2e0
Brennan McCreary <brennanmccreary@gmail.com>
2022-02-06 19:36:25 -0500

    Add comment, reply, like, and dislike functionality and API calls.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ import VideoPlayer from './VideoPlayer/VideoPlayer';
 import SearchResults from './SearchResults/SearchResults';
 import key from '../APIKey';
 import RelatedVideos from './RelatedVideos/RelatedVideos';
-import Test from './Test/Test';
 import Comments from './Comments/Comments';
 
 
@@ -16,9 +15,11 @@ class App extends Component {
         this.state = {
             search: '', //user inputted search term
             searchResults: [], //search results and data from youtube
-            relatedVideos: [],
-            comments: [],
-            selectedVideo: null
+            relatedVideos: [], //videos and data related to selected video
+            comments: [], //list of comments for selected video
+            selectedVideo: null, //video currently displayed in embedded player
+            replyText: '', //Text field for reply
+            commentText: '' //Text field for comment
         };
     }
 
@@ -30,49 +31,7 @@ class App extends Component {
                     console.log(res.data);
                 })
         };
-
-    ////HANDLERS
-    //update search bar 
-    handleChange = (event) => {
-        this.setState({
-            search: event.target.value
-        })
-    }
-
-    //handle submission of search result
-    handleSubmit = (event) => {
-        event.preventDefault();
-
-        this.getSearchResults(this.state.search, key)
-    }
-
-    //Handle video card being clicked
-    handleClickCard = (event) => {
-        let index = event.target.parentElement.id;
-        this.setState({
-            selectedVideo: this.state.searchResults[index]
-        })
-
-        console.log(this.state.searchResults[index].id.videoId);
-        this.getCommentsByVideoId(this.state.searchResults[index].id.videoId);
-        this.getRelatedVideos(this.state.searchResults[index].id.videoId, key);
-    }
-
-
-    //Handle related video card being clicked
-    handleClickRelatedCard = (event) => {
-        let index = event.target.parentElement.id;
-        this.setState({
-            selectedVideo: this.state.relatedVideos[index]
-        })
-
-        console.log(this.state.relatedVideos[index].id.videoId);
-        this.getCommentsByVideoId(this.state.relatedVideos[index].id.videoId);
-        this.getRelatedVideos(this.state.relatedVideos[index].id.videoId, key);
-    }
-
     
-
     //HTTP REQUESTS 
     //GET search results from Youtube
     getSearchResults = async (query, key) => {
@@ -115,10 +74,10 @@ class App extends Component {
     //POST Comment
     postComment = async (comment) => {
         await axios 
-                .get('http://localhost:5002/api/comments/add', comment)
+                .post('http://localhost:5002/api/comments/add', comment)
                 .then((res) => {
-                    this.setState({
-                        comments: [...prevState.comments, res.data]
+                    this.setState(prevState => {
+                        return { comments: [...prevState.comments, res.data] }
                     });
                 })
     }
@@ -126,10 +85,10 @@ class App extends Component {
     //PUT Like
     putLike = async (id) => {
         await axios
-                .get(`http://localhost:5002/api/comments/like/${id}`)
+                .put(`http://localhost:5002/api/comments/like/${id}`)
                 .then((res) => {
                     let target = [...this.state.comments];
-                    let index = target.findIndex(el => res.data._id === this.state.comments[el]._id);
+                    let index = target.findIndex((el) => res.data._id === el._id);
                     target[index] = res.data;
                     this.setState({
                         comments: target
@@ -140,10 +99,10 @@ class App extends Component {
     //PUT Dislike
     putDislike = async (id) => {
         await axios
-                .get(`http://localhost:5002/api/comments/dislike/${id}`)
+                .put(`http://localhost:5002/api/comments/dislike/${id}`)
                 .then((res) => {
                     let target = [...this.state.comments];
-                    let index = target.findIndex(el => res.data._id === this.state.comments[el]._id);
+                    let index = target.findIndex((el) => res.data._id === el._id);
                     target[index] = res.data;
                     this.setState({
                         comments: target
@@ -154,10 +113,10 @@ class App extends Component {
     //POST Reply
     postReplyToComment = async (id, reply) => {
         await axios
-                .get(`http://localhost:5002/api/comments/reply/${id}`, reply)
+                .post(`http://localhost:5002/api/comments/reply/${id}`, reply)
                 .then((res) => {
                     let target = [...this.state.comments];
-                    let index = target.findIndex(el => res.data._id === this.state.comments[el]._id);
+                    let index = target.findIndex(el => res.data._id === el._id);
                     target[index] = res.data;
                     this.setState({
                         comments: target
@@ -165,7 +124,8 @@ class App extends Component {
                 })
     }
     
-    //Functions
+    //Integrity and Handler Functions
+    //Check for incomplete HTTP responses
     checkDataIntegrity = (data) => {
         for(let i = 0; i < data.length; i++) {
             if(data[i].snippet === undefined) {
@@ -175,12 +135,111 @@ class App extends Component {
         return data;
     }
 
+    //update search bar 
+    handleChange = (event) => {
+        this.setState({
+            search: event.target.value
+        })
+    }
+
+    //handle submission of search result
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.getSearchResults(this.state.search, key)
+    }
+
+    //Handle video card being clicked
+    handleClickCard = (event) => {
+        let index = event.target.parentElement.id;
+        this.setState({
+            selectedVideo: this.state.searchResults[index]
+        })
+
+        document.getElementById("comments").hidden = false;
+
+        console.log(this.state.searchResults[index].id.videoId);
+        this.getCommentsByVideoId(this.state.searchResults[index].id.videoId);
+        this.getRelatedVideos(this.state.searchResults[index].id.videoId, key);
+    }
+
+
+    //Handle related video card being clicked
+    handleClickRelatedCard = (event) => {
+        let index = event.target.parentElement.id;
+        this.setState({
+            selectedVideo: this.state.relatedVideos[index]
+        })
+
+        console.log(this.state.relatedVideos[index].id.videoId);
+        this.getCommentsByVideoId(this.state.relatedVideos[index].id.videoId);
+        this.getRelatedVideos(this.state.relatedVideos[index].id.videoId, key);
+    }
+
+
+    //Handle submit comment
+    handleSubmitComment = (event) => {
+        event.preventDefault();
+
+        let comment = {
+            videoId: this.state.selectedVideo.id.videoId,
+            text: this.state.commentText
+        }
+
+        this.postComment(comment);
+
+        this.setState({
+            commentText: ''
+        })
+    };
+
+    //Handle submit reply
+    handleSubmitReply = (event) => {
+        event.preventDefault();
+
+        let reply = {text: this.state.replyText}
+        let id = event.target.parentElement.id
+
+        this.postReplyToComment(id, reply);
+
+        this.setState({
+            replyText: ''
+        })
+    };
+
+    //Update comment
+    onChangeComment = (event) => {
+        this.setState({
+            commentText: event.target.value
+        })
+    };
+
+    //Update reply
+    onChangeReply = (event) => {
+        this.setState({
+            replyText: event.target.value
+        })
+    };
+
+    //Handle click like
+    handleClickLike = (event) => {
+        let id = event.target.parentElement.id;
+
+        this.putLike(id);
+    };
+
+    //Handle click dislike
+    handleClickDislike = (event) => {
+        let id = event.target.parentElement.id;
+
+        this.putDislike(id);
+    };
+
     //Run When Component Initially Mounts
     componentDidMount() {
         this.getInit();
     };
 
-   
     //Render
     render() {
         return (
@@ -189,8 +248,7 @@ class App extends Component {
                 <SearchResults searchResults={this.state.searchResults} handleClick={this.handleClickCard} />
                 <VideoPlayer video={this.state.selectedVideo}/>
                 <RelatedVideos relatedVideos={this.state.relatedVideos} handleClick={this.handleClickRelatedCard}/>
-                <Comments comments={this.state.comments}/>
-                <Test/>
+                <Comments submitComment={this.handleSubmitComment} submitReply={this.handleSubmitReply} changeComment={this.onChangeComment} changeReply={this.onChangeReply} commentText={this.state.commentText} replyText={this.state.replyText} comments={this.state.comments} clickLike={this.handleClickLike} clickDislike={this.handleClickDislike}/>
             </div>
         )
     }
